cc370a8465fc3da2fd6ff7b5c1eff7d4d2372f73
Prince Odame <opodame@gmail.com>
2020-08-05 06:51:14 +0200

    Bootstrap NewConnection page

--- src/features/newConnection/NewConnection.tsx
+++ src/features/newConnection/NewConnection.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import ConnectionOptions from '../ConnectionOptions';
-import { Tabs } from 'antd';
+import React, { useState } from 'react';
+import ConnectionOptions from '../../components/ConnectionOptions';
+import { Tabs, Button, Form } from 'antd';
 import './NewConnection.css';
+import { DownloadOutlined } from '@ant-design/icons';
+import { useForm, FormInstance } from 'antd/lib/form/Form';
 
 const { TabPane } = Tabs;
 
 export default function NewConnection() {
+	const [form, setForm] = useState<FormInstance>();
+
+	const onClickConnect = async () => {
+		if (form === undefined) return;
+		try {
+			const values = await form.validateFields();
+		} catch (e) {
+			const error = e.errorFields as ReturnType<typeof form.getFieldsError>;
+			form.scrollToField(error[0].name, {
+				skipOverflowHiddenElements: true,
+				block: 'start',
+			});
+			console.error('Validation errors', error);
+		}
+	};
+
 	return (
 		<div className="page-content new-connection">
 			<Tabs
@@ -15,13 +33,21 @@ export default function NewConnection() {
 					<DefaultTabBar {...props} className="tabs-header" />
 				)}
 			>
-				<TabPane tab="Connection Options" key="options">
-					<ConnectionOptions />
+				<TabPane tab="Connection Options" key="options" className="tab-content">
+					<ConnectionOptions formRef={setForm} />
 				</TabPane>
 				<TabPane tab="Variables" key="variables">
 					Variables
 				</TabPane>
 			</Tabs>
+			<Button
+				className="action-button"
+				type="primary"
+				shape="circle"
+				icon={<DownloadOutlined />}
+				size={'large'}
+				onClick={onClickConnect}
+			/>
 		</div>
 	);
 }
