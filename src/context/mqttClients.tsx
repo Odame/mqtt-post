8e1b6076037be606ac68f7caa5fce9df34f3e0b4
Prince Odame <opodame@gmail.com>
2020-09-11 00:23:58 +0200

    Implements hook for listening to mqtt client's state

--- src/context/mqttClients.tsx
+++ src/context/mqttClients.tsx
@@ -108,3 +108,64 @@ export const useMqttClient = (connectionId: string) => {
 
 	return memoizedClient;
 };
+
+export enum MqttClientState {
+	/** The exact state of the mqtt client cannot be determined currently */
+	unknown,
+	/** On successful (re)connection, (i.e. connack rc=0) */
+	connected,
+	/** After receiving disconnect packet from broker. MQTT 5.0 feature */
+	disconnected,
+	/** When a reconnect starts */
+	reconnecting,
+	/** When mqtt.Client goes offline */
+	offline,
+	/** When mqtt.Client.end is called */
+	ended,
+}
+
+/** Hook to track the state of a connection's related mqtt client */
+export const useMqttClientState = (connectionId: string) => {
+	const [clientState, setClientState] = useState<MqttClientState>(
+		MqttClientState.unknown
+	);
+	const [clientError, setClientError] = useState<Error | null>(null);
+
+	const client = useMqttClient(connectionId);
+	useEffect(() => {
+		if (!client) return;
+
+		const onConnect = () => {
+			setClientState(MqttClientState.connected);
+			// The client has successfully connected, so clear the error
+			setClientError(null);
+		};
+		const onDisconnect = () => setClientState(MqttClientState.disconnected);
+		const onOffline = () => setClientState(MqttClientState.offline);
+		const onReconnect = () => setClientState(MqttClientState.reconnecting);
+		const onEnd = () => {
+			setClientState(MqttClientState.ended);
+			// The client has been ended, the error state makes sense no more
+			setClientError(null);
+		};
+
+		client
+			.addListener('connect', onConnect)
+			.addListener('disconnect', onDisconnect)
+			.addListener('offline', onOffline)
+			.addListener('reconnect', onReconnect)
+			.addListener('end', onEnd)
+			.addListener('error', setClientError);
+
+		return () => {
+			client
+				.removeListener('connect', onConnect)
+				.removeListener('disconnect', onDisconnect)
+				.removeListener('offline', onOffline)
+				.removeListener('end', onEnd)
+				.removeListener('error', setClientError);
+		};
+	}, [client]);
+
+	return { clientState, clientError };
+};
