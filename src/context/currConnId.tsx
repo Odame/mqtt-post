84194248e91d7b4f4da656eed192454e1cfd4f3b
Prince Odame <opodame@gmail.com>
2020-09-11 00:23:33 +0200

    Typings for slected connection context

--- src/context/currConnId.tsx
+++ src/context/currConnId.tsx
@@ -24,5 +24,5 @@ export const useSelectedConnectionId = () => {
 /** Hook which returns a function that sets the currently selected connection's id */
 export const useCurrConnectionIdSetter = () => {
 	const setCurrConnectionId = useContext(CurrConnectionIdSetterContext);
-	return setCurrConnectionId;
+	return setCurrConnectionId!;
 };
