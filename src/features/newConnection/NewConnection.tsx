2a66ced15e22ff1a966944209db0bf53f73fe9c9
Prince Odame <opodame@gmail.com>
2020-09-13 23:44:48 +0200

    Implements NewConnection component with TabbedPages

--- src/features/newConnection/NewConnection.tsx
+++ src/features/newConnection/NewConnection.tsx
@@ -1,40 +1,20 @@
-import React, { useState } from 'react';
-import ConnectionOptions, {
-	IConnectionOptions,
-} from '../../components/ConnectionOptions';
-import { Tabs, Button, Form } from 'antd';
+import React from 'react';
+import ConnectionOptions from '../../components/ConnectionOptions';
+import { Button } from 'antd';
 import './NewConnection.css';
-import { DownloadOutlined, SaveOutlined } from '@ant-design/icons';
-import { useForm, FormInstance } from 'antd/lib/form/Form';
-import { IClientOptions } from 'mqtt';
-import EnvironmentVariables, {
-	IVar,
-} from '../../components/Variables/EnvironmentVariables';
+import { SaveOutlined } from '@ant-design/icons';
+import { useForm } from 'antd/lib/form/Form';
+import { getDatabase } from '../../db';
+import { IConnection } from '../../db/schemas/connection';
+import TabbedPage from '../../containers/pageWithTabs/TabbedPage';
 
-const { TabPane } = Tabs;
-
-const formValuesToClientOptions = (formValues: IConnectionOptions) => {
-	return {
-		...(formValues as IClientOptions),
-		protocolId: formValues.protocolVersion === 3 ? 'MQIsdp' : 'MQTT',
-		path: ['ws', 'wss'].includes(formValues.protocol) ? formValues.path : '',
-	} as IClientOptions;
-};
-
-enum TabKey {
-	ConnectionOptions = 'connectionOptions',
-	Variables = 'variable',
-}
 export default function NewConnection() {
-	const [activeTab, setActiveTab] = useState<TabKey>(TabKey.ConnectionOptions);
 	const [connectionOptionsForm] = useForm();
-	const [environmentVariablesForm] = useForm();
 
-	const onClickConnect = async () => {
-		let connectionOptions: IConnectionOptions;
-		let variables: Array<IVar>;
+	const onClickSave = async () => {
+		let connectionOptions: Omit<IConnection, 'id'>;
 		try {
-			connectionOptions = (await connectionOptionsForm.validateFields()) as IConnectionOptions;
+			connectionOptions = (await connectionOptionsForm.validateFields()) as IConnection;
 		} catch (e) {
 			const error = e.errorFields as ReturnType<
 				typeof connectionOptionsForm.getFieldsError
@@ -43,65 +23,30 @@ export default function NewConnection() {
 				skipOverflowHiddenElements: true,
 				block: 'start',
 			});
-			setActiveTab(TabKey.ConnectionOptions);
 			return;
 		}
-
-		try {
-			const values = await environmentVariablesForm.validateFields();
-			variables = values['nameValuePairs'] as Array<IVar>;
-		} catch (e) {
-			const error = e.errorFields as ReturnType<
-				typeof environmentVariablesForm.getFieldsError
-			>;
-			environmentVariablesForm.scrollToField(error[0].name, {
-				skipOverflowHiddenElements: true,
-				block: 'start',
-			});
-			setActiveTab(TabKey.Variables);
-			return;
-		}
+		getDatabase().connections.upsertConnection(connectionOptions);
 	};
 
 	return (
-		<div className="page-content new-connection">
-			<Tabs
-				className="tabs-container"
-				activeKey={activeTab}
-				renderTabBar={(props, DefaultTabBar) => (
-					<DefaultTabBar {...props} className="tabs-header" />
-				)}
-				onChange={(activeKey) => setActiveTab(activeKey as TabKey)}
-			>
-				<TabPane
-					tab="Connection Options"
-					key={TabKey.ConnectionOptions}
-					className="tab-content"
-				>
-					<ConnectionOptions form={connectionOptionsForm} />
-				</TabPane>
-				<TabPane tab="Variables" key={TabKey.Variables} className="tab-content">
-					<EnvironmentVariables
-						form={environmentVariablesForm}
-						initialNameValuePairs={
-							[
-								// { varName: 'one', varValue: '1' },
-								// { varName: 'two', varValue: '2' },
-								// { varName: 'three', varValue: '3' },
-								// { varName: 'four', varValue: '4' },
-							]
-						}
-					/>
-				</TabPane>
-			</Tabs>
-			<Button
-				className="action-button"
-				type="primary"
-				shape="circle"
-				icon={<SaveOutlined size={1} />}
-				size={'large'}
-				onClick={onClickConnect}
-			/>
-		</div>
+		<TabbedPage
+			className="new-connection"
+			tabs={{
+				newConnection: [
+					'New Connection',
+					<ConnectionOptions form={connectionOptionsForm} />,
+				],
+			}}
+			extraContent={
+				<Button
+					className="floating-action-button"
+					type="primary"
+					shape="circle"
+					icon={<SaveOutlined />}
+					size={'large'}
+					onClick={onClickSave}
+				/>
+			}
+		/>
 	);
 }
